7c517bf9d45bdc9142235b375cf279eca6b034c4
dns <dns@LAPTOP-S9A7CAQR>
2024-06-01 19:27:59 +0300

    Initial Commit

---
+++ test/test_urm.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestUrm < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Urm::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
