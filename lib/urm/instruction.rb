0bec59a1bfd89a078e14221fa249ffd1764f205b
YBarsukova <yaroslavabarsukova@internet.ru>
2024-06-02 14:14:39 +0300

    Added copy instruction type (syntax sugar)

--- lib/urm/instruction.rb
+++ lib/urm/instruction.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require "urm/exceptions"
+require "exceptions"
 
 module Urm
   # This class represents an instruction for the Unbounded Register Machine (URM).
   # It includes various types of instructions such as setting a register,
-  # incrementing a register, decrementing a register, conditional jumps, and stop.
+  # incrementing a register, decrementing a register, conditional jumps, stop, and copy.
   class Instruction
     attr_reader :type, :register, :value, :label_true, :label_false
 
@@ -47,6 +47,12 @@ module Urm
       new(:stop)
     end
 
+    def self.copy(register, source_register)
+      raise Urm::InvalidRegisterIndex, "Register index must be positive" if register <= 0 || source_register <= 0
+
+      new(:copy, register: register, value: source_register)
+    end
+
     def to_s
       case @type
       when :set
@@ -59,6 +65,8 @@ module Urm
         "if x#{@register} == 0 goto #{@label_true} else goto #{@label_false}"
       when :stop
         "stop"
+      when :copy
+        "x#{@register} = x#{@value}"
       else
         raise "Unknown instruction type: #{@type}"
       end
@@ -73,6 +81,8 @@ module Urm
         parse_dec(match)
       elsif (match = instruction_str.match(/^if x(\d+) == 0 goto (\d+) else goto (\d+)$/))
         parse_if(match)
+      elsif (match = instruction_str.match(/^x(\d+) = x(\d+)$/))
+        parse_copy(match)
       elsif instruction_str == "stop"
         parse_stop
       else
@@ -97,6 +107,10 @@ module Urm
 
       def parse_if(match)
         self.if(match[1].to_i, match[2].to_i, match[3].to_i)
+      end
+
+      def parse_copy(match)
+        copy(match[1].to_i, match[2].to_i)
       end
 
       def parse_stop
--- test/test_instruction.rb
+++ test/test_instruction.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
-require "urm/instruction"
-require "urm/exceptions"
+require "instruction"
+require "exceptions"
 
 class TestInstruction < Minitest::Test
   def test_set_instruction
