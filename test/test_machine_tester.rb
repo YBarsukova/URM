b187a9fa5389480e1a6e04db3992ae4e51d36826
YBarsukova <yaroslavabarsukova@internet.ru>
2024-06-02 19:05:18 +0300

    Added division, adding and subtraction tests for MachineTester

--- test/test_machine_tester.rb
+++ test/test_machine_tester.rb
@@ -23,6 +23,58 @@ class TestMachineOperations < Minitest::Test
     machine = Urm::Machine.new(2)
     machine.add_all(multiplication_instructions)
 
-    MachineTester.assert_range(machine, 0, 100, lambda { |x2, x3| x2 * x3 })
+    MachineTester.assert_range(machine, 0, 10, lambda { |x2, x3| x2 * x3 })
   end
+
+  def test_division
+    division_instructions = [
+      "1. if x2 == 0 goto 10 else goto 2",
+      "2. x4 = x3",
+      "3. x2 = x2 - 1",
+      "4. x4 = x4 - 1",
+      "5. if x2 == 0 goto 6 else goto 7",
+      "6. if x4 == 0 goto 8 else goto 10",
+      "7. if x4 == 0 goto 8 else goto 3",
+      "8. x1 = x1 + 1",
+      "9. if x2 == 0 goto 10 else goto 2",
+      "10. stop"
+    ]
+    machine = Urm::Machine.new(2)
+    machine.add_all(division_instructions)
+
+    MachineTester.assert_range(machine, 1, 10, lambda { |x2, x3| x2 / x3 })
+  end
+
+  def test_subtraction
+    subtraction_instructions = [
+      "1. if x2 == 0 goto 7 else goto 2",
+      "2. if x3 == 0 goto 6 else goto 3",
+      "3. x2 = x2 - 1",
+      "4. x3 = x3 - 1",
+      "5. if x2 == 0 goto 7 else goto 2",
+      "6. x1 = x2",
+      "7. stop"
+    ]
+
+    machine = Urm::Machine.new(2)
+    machine.add_all(subtraction_instructions)
+
+    MachineTester.assert_range(machine, 0, 100, lambda { |x2, x3| [x2 - x3, 0].max })
+  end
+
+  def test_addition
+    addition_instructions = [
+      "1. if x2 == 0 goto 5 else goto 2",
+      "2. x2 = x2 - 1",
+      "3. x1 = x1 + 1",
+      "4. if x2 == 0 goto 5 else goto 2",
+      "5. stop",
+    ]
+
+    machine = Urm::Machine.new(2)
+    machine.add_all(addition_instructions)
+
+    MachineTester.assert_range(machine, 0, 100, lambda { |x2, x3| x2 + x3 })
+  end
+
 end
